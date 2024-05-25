14b917ddcf18f57dd4762af1af2ac6f97d78a15a
jyoti prakash dey <jyotiprakashdey@JPD.lan>
2024-05-25 22:12:28 +0530

    new changes

---
+++ src/cnnClassifier/utils/common.py
@@ -0,0 +1,15 @@
+import os
+import yaml
+from cncClassifier import logger
+import json
+import joblib
+from ensure import ensure_annotations
+from box import ConfigBox
+from typing import Any
+import base64
+from box.exceptions import BoxValuerError
+
+
+
+
+
--- template.py
+++ template.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 import logging
 logging.basicConfig(level=logging.INFO,format='[%(asctime)s]: %(message)s:')
-project_name="cncClassifier"
+project_name="cnnClassifier"
 
 list_of_files=[
     ".github/workflows/.gitkeep",
